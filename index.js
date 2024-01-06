6f895ed4896019ff0a4bf980a6a8003732cf6cac
Thanthiti <plam1234palm@gmail.com>
2024-01-06 21:46:36 +0700

    Test

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-require("dotenv").config()
+require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000 ;
 
-app.get("/", (req, res) =>{
-  res.send("Hello world")
-  
+app.get("/", (req, res) => {
+  res.send("Hello world");
 });
 
-app.listen( port,() => {
-    console.log(`Example app listing at http://localhost:${port}`);
-  },
-);
+app.listen(port, () => {
+  console.log(`Example app listing at http://localhost:${port}`);
+});
